8676a618200fd71cba35e944d1d2f3e092160afc
Martin Guevara <martin_91@live.com.ar>
2022-08-26 13:06:02 -0300

    Desafio: Item Collection 2

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Todas las entregas de los desafíos serán pusheados en este repositorio.<br>
 La tienda se encuentra realizada con React utilizando como base <b>create-react-app</b> y <b>Material UI</b> para la parte visual y sus componentes estilizados.
+
+## What's next?
+- Incluir alert al sumar artículos al carrito
+- Agregar secciones faltantes en la página
+- Mejorar estética general
--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import './checkout.css'
 
 function Checkout() {
   return (
-    <div>Checkout</div>
+    <div className='checkout__container'>
+      <div className='checkout__title'>
+        <h1>Checkout</h1>
+        <p>Por favor complete los datos para finalizar la compra.</p>
+      </div>
+      <div className='checkout__form'>
+        <div className='checkout__form--fields'>1</div>
+        <div className='checkout__form--cart'>2</div>
+      </div>
+    </div>
   )
 }
 
